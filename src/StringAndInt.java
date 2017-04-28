a8a10bf98163ae12f0c14d9ec5317107a1415bcd
karim nadji <knadji@norsys.fr>
2017-04-28 09:57:49 +0200

    Initialisation du dépôt BigData

---
+++ src/StringAndInt.java
@@ -0,0 +1,25 @@
+
+
+public class StringAndInt implements Comparable<StringAndInt> {
+	
+	private String tag;
+	private double nbOccurTag;
+	
+
+	@Override
+	public int compareTo(StringAndInt o) {
+		if(this.nbOccurTag > o.nbOccurTag){
+			return 1;
+		}else if (this.nbOccurTag < o.nbOccurTag){
+			return -1; 
+		}
+		return 0;
+	}
+
+
+	@Override
+	public String toString() {
+		return "[tag=" + tag + ", nbOccurTag=" + nbOccurTag + "]";
+	}
+	
+}
